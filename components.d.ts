93c13ccc36014c6eb32f8fc93e6f8214000f7ad0
donghy1 <915110101@qq.com>
2022-03-25 16:18:52 +0800

    ✨新增(Vue): 完成main页面的样式开发

--- components.d.ts
+++ components.d.ts
@@ -4,16 +4,31 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
+    ElCol: typeof import('element-plus/es')['ElCol']
+    ElContaine: typeof import('element-plus/es')['ElContaine']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElContianer: typeof import('element-plus/es')['ElContianer']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElLink: typeof import('element-plus/es')['ElLink']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElMenuItemGroup: typeof import('element-plus/es')['ElMenuItemGroup']
+    ElSubmenu: typeof import('element-plus/es')['ElSubmenu']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    NavHeader: typeof import('./src/components/nav-header/src/nav-header.vue')['default']
+    NavMain: typeof import('./src/components/nav-main/src/nav-main.vue')['default']
+    NavMenu: typeof import('./src/components/nav-menu/src/nav-menu.vue')['default']
   }
 }
 
---
+++ src/components/nav-header/src/nav-header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="nav-header">
+        <svg
+            t="1648191030569"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="1987"
+            width="20"
+            height="20"
+            @click="handleClick"
+        >
+            <path
+                d="M96 160h832c19.2 0 32 12.8 32 32s-12.8 32-32 32h-832c-19.2 0-32-12.8-32-32s12.8-32 32-32z m0 320h832c19.2 0 32 12.8 32 32s-12.8 32-32 32h-832c-19.2 0-32-12.8-32-32s12.8-32 32-32z m0 320h832c19.2 0 32 12.8 32 32s-12.8 32-32 32h-832c-19.2 0-32-12.8-32-32s12.8-32 32-32z"
+                p-id="1988"
+            />
+        </svg>
+    </div>
+</template>
+
+<script setup>
+import {ref,defineEmits} from 'vue'
+const isFold = ref(false)
+const handleClick = ()=>{
+    isFold.value = !isFold.value
+    emits('foldChange',isFold.value)
+}
+const emits = defineEmits(['foldChange'])
+</script>
+
+<style lang="less" scoped>
+</style>
